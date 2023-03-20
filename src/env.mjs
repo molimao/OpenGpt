c9fa1ed8b8302abbc5d1e4a813ad0c5415c5d80f
魔力猫 <47518904+molimao@users.noreply.github.com>
2023-03-20 12:38:29 +0800

    Update env.mjs

--- src/env.mjs
+++ src/env.mjs
@@ -42,7 +42,7 @@ const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL!,
   NODE_ENV: process.env.NODE_ENV!,
   // TODO: uncomment once login is enabled
-  // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID!,
   // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET!,
