e2bc937e5136d14f307fe44c7e35c1f45ccdeae1
魔力猫 <47518904+molimao@users.noreply.github.com>
2023-03-20 12:35:39 +0800

    Update env.mjs
    try update

--- src/env.mjs
+++ src/env.mjs
@@ -8,20 +8,20 @@ const server = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
   // TODO: uncomment once login is enabled
-  // NEXTAUTH_SECRET:
-  //   process.env.NODE_ENV === 'production'
-  //     ? z.string(123465).min(1)
-  //     : z.string(123456).min(1).optional(),
-  // NEXTAUTH_URL: z.preprocess(
-  //   // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-  //   // Since NextAuth.js automatically uses the VERCEL_URL if present.
-  //   (str) => process.env.VERCEL_URL ?? str,
-  //   // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-  //   process.env.VERCEL ? z.string().min(1) : z.string().url()
-  // ),
+  NEXTAUTH_SECRET:
+     process.env.NODE_ENV === 'production'
+       ? z.string(123456).min(1)
+       : z.string(123456789).min(1).optional(),
+  NEXTAUTH_URL: z.preprocess(
+     // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+     // Since NextAuth.js automatically uses the VERCEL_URL if present.
+     (str) => process.env.VERCEL_URL ?? str,
+     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+     process.env.VERCEL ? z.string().min(1) : z.string().url()
+   ),
   // // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-  // DISCORD_CLIENT_ID: z.string(123456).min(1),
-  // DISCORD_CLIENT_SECRET: z.string(123456).min(1),
+  // DISCORD_CLIENT_ID: z.string(),
+  // DISCORD_CLIENT_SECRET: z.string(),
 })
 
 /**
@@ -29,7 +29,7 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(1234569898).min(1),
+   NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
 })
 
 /**
@@ -39,14 +39,14 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
-  NODE_ENV: process.env.NODE_ENV,
+  DATABASE_URL: process.env.DATABASE_URL!,
+  NODE_ENV: process.env.NODE_ENV!,
   // TODO: uncomment once login is enabled
-  // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID!,
+  // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET!,
+  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR!,
 }
 
 // Don't touch the part below
